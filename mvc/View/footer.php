11607351fd20d3a724391af088ad680f81bfa54c
Josaphat Lopez Ceron <josa1980@gmail.com>
2021-04-19 17:23:30 -0600

    creacion de footer

---
+++ mvc/View/footer.php
@@ -0,0 +1,28 @@
+            <div class="row">
+                <div class="col-xs-12">
+                    <hr />
+
+                    <footer class="footer-distributed">
+
+			                <div class="footer-right">
+				                
+  			              </div>
+
+			                <div class="footer-left">
+				                <p class="footer-links">
+					                <a href="index.php">Inicio</a>
+				                </p>
+				                <p>Desarrollo de Sistemas informáticos web I UTEC 2021</p>
+			                </div>
+		                </footer>
+                </div>
+            </div>
+        </div>
+
+        <script src="assets/js/jquery-1.11.2.min.js"></script>
+        <script src="assets/js/bootstrap.min.js"></script>
+        <script src="assets/js/jquery-ui/jquery-ui.min.js"></script>
+        <script src="assets/js/ini.js"></script>
+        <script src="assets/js/jquery.anexsoft-validator.js"></script>
+    </body>
+</html>
